9d7000d56af1d8f8cebc82575bea7f90a053d80b
Phil Parsons <phil@profilepicture.co.uk>
2011-08-14 17:52:38 +0100

    adding high score db stuff

--- server/config.php
+++ server/config.php
@@ -1,6 +1,6 @@
 <?php 
 
-	define("DB_USER", "");
+	define("DB_USER", "root");
 	define("DB_PASS", "");
-	define("DB_HOST", "");
-	define("DB_SCHEMA", "");
+	define("DB_HOST", "localhost");
+	define("DB_SCHEMA", "pong_turbo");
--- server/high_scores.php
+++ server/high_scores.php
@@ -1,21 +1,41 @@
 <?php
 
+	require 'config.php';
+	$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_SCHEMA);
+
 	if (array_key_exists("recordScore", $_POST)) {
 		
-		require 'config.php';
-		$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_SCHEMA);
 		$stmt = $conn->prepare(
-			'INSERT INTO score (username, score, played_on_date) VALUES (?, ?, NOW())'
+			'INSERT INTO score (username, score, played_on_date)' .
+			'VALUES (?, ?, NOW())'
 		);
 		if ($stmt) {
 			$stmt->bind_param('ss', $_POST['username'], $_POST['score']);
 			$stmt->execute();
 			$stmt->close();
 		}
-		$conn->close();
 
 	} else if (array_key_exists("getScores", $_GET)) {
-		
-		// get high scores
+
+		$ret = array();
+		$stmt = $conn->prepare(
+			'SELECT username, score FROM scores ' .
+			'ORDER BY score LIMIT 10'
+		);
+		if ($stmt) {
+			$stmt->execute();
+			$stmt->bind_result($user, $score);
+			while ($stmt->fetch()) {
+				array_push($ret, array(
+						'user' => $user
+					,	'score' => $score
+				));
+			}
+			header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+			header('Content-Type:application/json');
+			echo json_encode($ret);
+		}
 
 	}
+		
+	$conn->close();
